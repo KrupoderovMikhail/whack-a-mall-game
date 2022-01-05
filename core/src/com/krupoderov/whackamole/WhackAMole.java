5d2fc92a1cf4de21a80272931e00567f7c1f74f4
Круподеров Михаил Сергеевич <krupoderov@proitr.ru>
2022-01-05 12:16:43 +0500

    feat: add the previously created moles to the main screen

--- core/src/com/krupoderov/whackamole/WhackAMole.java
+++ core/src/com/krupoderov/whackamole/WhackAMole.java
@@ -1,31 +1,48 @@
 package com.krupoderov.whackamole;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.krupoderov.whackamole.managers.GameManager;
 
 public class WhackAMole extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+    SpriteBatch batch; // spritebatch for drawing
+    OrthographicCamera camera;
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    @Override
+    public void create() {
+        // get window dimension and set out viewport dimensions
+        float height = Gdx.graphics.getHeight();
+        float width = Gdx.graphics.getWidth();
+        // set out camera viewport to window dimensions
+        camera = new OrthographicCamera(width, height);
+        // center to the camera at w/2, h/2
+        camera.setToOrtho(false);
+        batch = new SpriteBatch();
+        // initialize the game
+        GameManager.initialize(width, height);
+    }
+
+    @Override
+    public void render() {
+        // clear the screen
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        // set the spritebatch's drawing view to the camera's view
+        batch.setProjectionMatrix(camera.combined);
+
+        // render the game objects
+        batch.begin();
+        GameManager.renderGame(batch);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        // dispose the batch and the textures
+        batch.dispose();
+        GameManager.dispose();
+    }
 }
